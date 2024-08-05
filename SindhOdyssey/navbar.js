56f0dcbede9b1c79104b60607017043b1742c5c0
Shehnila Narejo <129547364+shehnilanarejo@users.noreply.github.com>
2024-08-06 01:42:15 +0500

    Add files via upload

---
+++ SindhOdyssey/navbar.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        });
+});
+
